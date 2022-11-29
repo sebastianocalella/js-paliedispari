1bf70c70646b55b8a391a15a9f1d2598d1188643
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-29 16:12:27 +0100

    first steps

---
+++ odd or even/script.js
@@ -0,0 +1,11 @@
+const randomValue = Math.floor(Math.random()*5 + 1);
+
+console.log(randomValue);
+
+const start = document.getElementById('play');
+
+start.addEventListener(click,oddOrEven);
+
+function oddOrEven(){
+    
+}

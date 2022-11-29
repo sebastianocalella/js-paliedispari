5c0a282867d3b48651977c35d73bdd9a79313703
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-29 17:03:39 +0100

    structure kind of

--- odd or even/script.js
+++ odd or even/script.js
@@ -1,6 +1,3 @@
-const randomValue = Math.floor(Math.random()*5 + 1);
-
-const start = document.getElementById('play');
 
 function isOdd(number){
     if (number%2 === 1){
@@ -9,18 +6,25 @@ function isOdd(number){
     return false
 }
 
-const example = 4;
-const sum = randomValue + example;
-const isoddtest = isOdd(sum);
 
-console.log(randomValue);
-console.log(example);
-console.log(sum);
-console.log(isoddtest);
+const start = document.getElementById('play');
 
+start.addEventListener('click', function(){
 
+    const randomValue = Math.floor(Math.random()*5 + 1);
+    const userNumber = parseInt(document.getElementById('user-number-input').value);
+    const sum = randomValue + userNumber;
 
-
+    console.log(userNumber);
+    if (!(userNumber>0 && userNumber<=5)){
+        document.getElementById('answer').innerHTML = 'per favore inserisci un numero compreso fra 1 e 5';
+    } else if ((document.getElementById('odd').checked && isOdd(sum)) || (document.getElementById('even').checked && !isOdd(sum))){
+        console.log('oh yeah');
+        document.getElementById('answer').innerHTML = 'complimenti hai vinto!';
+    } else {
+        document.getElementById('answer').innerHTML = 'mi spiace ma hai perso :P';
+    }
+});
 
 
 

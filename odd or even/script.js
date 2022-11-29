83c3f4c3e60203151695e6d7cd047ecf8fb0c295
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-29 16:24:22 +0100

    isOdd function

--- odd or even/script.js
+++ odd or even/script.js
@@ -4,8 +4,26 @@ console.log(randomValue);
 
 const start = document.getElementById('play');
 
-start.addEventListener(click,oddOrEven);
-
-function oddOrEven(){
-    
+function isOdd(number){
+    if (number%2 === 1){
+        return true
+    }
+    return false
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// start.addEventListener(click,oddOrEven);
+
+// function oddOrEven(number){
+//     const userValue = parseInt(document.getElementById('user-number-input').value, 10);
+// }
